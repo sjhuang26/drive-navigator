ea27130d95998011afa4590bccadbdc939c5a6e3
sjhuang26 <sjhuang26@gmail.com>
2017-12-24 13:24:35 -0500

    Wrote test

---
+++ scripts/app.js
@@ -0,0 +1,51 @@
+requirejs.config({
+  shim: {
+    "bootstrap": {
+      deps: ["jquery", "popper-loader"]
+    },
+    "gapi": {
+      exports: "gapi"
+    },
+    "storejs": {
+      exports: "store"
+    },
+    "underscorejs": {
+      exports: "_"
+    },
+    "vue": {
+      exports: "Vue"
+    },
+    "popperjs": {
+      deps: ["jquery"],
+      init: function(p) {
+        window.Popper = p;
+      }
+    }
+  },
+  paths: {
+    "bootstrap": "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min",
+    "gapi": "https://apis.google.com/js/api",
+    "google-drive": "lib/google-drive-util/google-drive",
+    "jquery": "https://code.jquery.com/jquery-3.2.1.min",
+    "popperjs": "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min",
+    "popper-loader": "lib/popper-loader",
+    "storejs": "https://cdn.rawgit.com/marcuswestin/store.js/2b486f1f/dist/store.modern.min",
+    "underscorejs": "http://underscorejs.org/underscore-min",
+    "vue": "https://vuejs.org/js/vue",
+  }
+});
+
+requirejs(["jquery", "vue", "underscorejs", "google-drive", "bootstrap"], function($, Vue, _, drive) {
+  $(() => {
+    var driveInstance = new drive.GoogleDrive({
+      CLIENT_ID: '234321269537-fhsu1deau3cu4j3bjuqm2scn992tu30l.apps.googleusercontent.com',
+      API_KEY: 'AIzaSyCRk56putTEtlFVOQDGHXu6hFcz902kI9o',
+      DISCOVERY_DOCS: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
+      SCOPES: 'https://www.googleapis.com/auth/drive.metadata.readonly'
+    });
+    
+    driveInstance.init().then(() => {
+      driveInstance.test();
+    });
+  });
+});
---
+++ scripts/lib/google-drive-util/google-drive.js
@@ -0,0 +1,67 @@
+define(["gapi"], function(gapi) {
+  var r = {};
+
+  r.GoogleDrive = class GoogleDrive {
+    constructor(config) {
+      this.CLIENT_ID = config.CLIENT_ID;
+      this.API_KEY = config.API_KEY;
+      this.DISCOVERY_DOCS = config.DISCOVERY_DOCS;
+      this.SCOPES = config.SCOPES;
+      this.gapi = gapi;
+    }
+
+    init() {
+      return new Promise((resolve, reject) => {
+        gapi.load('client:auth2', () => {
+          gapi.client.init({
+            apiKey: this.API_KEY,
+            clientId: this.CLIENT_ID,
+            discoveryDocs: this.DISCOVERY_DOCS,
+            scope: this.SCOPES
+          }).then(() => {
+            // Listen for sign-in state changes.
+            gapi.auth2.getAuthInstance().isSignedIn.listen(this.onSignInStatusChange);
+    
+            // Handle the initial sign-in state.
+            this.onSignInStatusChange(gapi.auth2.getAuthInstance().isSignedIn.get());
+
+            resolve();
+          });
+        });
+      });
+    }
+
+    onSignInStatusChange(isSignedIn) {
+      console.log("isSignedIn =", isSignedIn);
+    }
+
+    signIn() {
+      gapi.auth2.getAuthInstance().signIn();
+    }
+
+    signOut() {
+      gapi.auth2.getAuthInstance().signOut();
+    }
+
+    test() {
+      gapi.client.drive.files.list({
+        "orderBy": "viewedByMeTime desc",
+        "pageSize": 10,
+        "fields": "files(id, name)"
+      }).then(function(response) {
+        console.log("Files found:");
+        var files = response.result.files;
+        if (files && files.length > 0) {
+          for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            console.log(file.name + " (" + file.id + ")");
+          }
+        } else {
+          console.log("No files found.");
+        }
+      });
+    }
+  }
+
+  return r;
+});
